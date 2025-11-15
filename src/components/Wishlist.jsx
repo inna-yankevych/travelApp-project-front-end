069138bcc581459fbce50d54d6b090bf77c9fb90
Inna Yankevych <inna.yankevych@gmail.com>
2025-11-15 22:27:43 +1100

    Deleted duplicate-ish wishlist hooks file, and removed redundant import

--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { getAllWishlists, createWishlist, deleteWishlistById } from "../apiEndpoints/wishlistApi";
-import { useWishlist, useWishlists } from "../hooks/useWishlist";
 
 export default function Wishlist() {
   const [wishlist, setWishlist] = useState([]);
