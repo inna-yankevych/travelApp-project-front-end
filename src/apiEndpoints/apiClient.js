dcf06032393b584d5a76cf6519e764062fd2276b
Kate Mendoza <16334@coderacademy.edu.au>
2026-02-14 23:25:53 +1100

    remove bearer token

--- src/apiEndpoints/apiClient.js
+++ src/apiEndpoints/apiClient.js
@@ -15,7 +15,7 @@ apiClient.interceptors.request.use(config => {
   // const token = localStorage.getItem("token");
   // console.log("token", token)
   // if (token) {
-    config.headers.Authorization = `Bearer ${BEARER_TOKEN}`;
+    // config.headers.Authorization = `Bearer ${BEARER_TOKEN}`;
   // }
   return config;
 })
